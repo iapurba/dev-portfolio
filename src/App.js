2cf2d5c4850aac1df5098181393cbf8bc1f5302e
Apurba Panja <apurbapanja1@gmail.com>
2020-11-26 17:34:43 +0530

    Included newly created componenets to the App componenet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/navigation/Navigation';
+import HeroSection from './components/heroSection/HeroSection';
+import About from './components/about/About';
+import SkillSet from './components/skillSet/SkillSet';
+import Project from './components/project/Project';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import Logo from './components/logo/Logo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <HeroSection />
+      <About />
+      <SkillSet />
+      <Project />
+      <footer>
+      <Logo/>
+      <Row className='justify-content-md-center'>
+        <Col md={4}>
+          <h3>Living, learning, & leveling up one day at a time.</h3>
+        </Col>
+      </Row>
+      </footer>
     </div>
   );
 }
