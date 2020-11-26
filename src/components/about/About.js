77c4d16a6562d40b8ccf8f2095354a025be55078
Apurba Panja <apurbapanja1@gmail.com>
2020-11-26 17:38:34 +0530

    Created About  component

---
+++ src/components/about/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './About.css';
+import { Container } from 'react-bootstrap';
+
+
+const About = () => {
+  return (
+    <section className='about is-colored'>
+      <Container>
+        <div className='content'>
+          <h2 className='title'><strong>Hi there, I'm Apurba. Nice to meet you.</strong></h2>
+          <p className='subtitle'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.
+
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.</p>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+
+export default About;
