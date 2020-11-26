1eef23b57b01f2997f45b67219164739db7a53cb
Apurba Panja <apurbapanja1@gmail.com>
2020-11-26 17:38:17 +0530

    Created Hero  component

---
+++ src/components/heroSection/HeroSection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import profileImage from './profileImage.jpg';
+import './HeroSection.css';
+import { Container } from 'react-bootstrap';
+
+
+const HeroSection = () => {
+  return (
+    <section className='hero'>
+      <Container>
+        <div className='hero-body'>
+            <div>
+              <h1 className='title'><strong>Full Stack Developer </strong></h1>
+              <h3 className='subtitle'> I build complete website and I love what I do. </h3>
+            </div>
+            <img className='profile-avatar rounded-circle' alt='profile' src={profileImage} />
+        </div>
+        <div className='hero-footer'>
+
+        </div>
+      </Container>
+    </section>
+  );
+}
+
+
+export default HeroSection;
