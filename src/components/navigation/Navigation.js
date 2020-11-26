287a1c36662ae51ec9ac02c39855c93427581a0a
Apurba Panja <apurbapanja1@gmail.com>
2020-11-26 17:37:53 +0530

    Created Navigation  component

---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,22 @@
+import Logo from '../logo/Logo';
+import './Navigation.css';
+import { Container } from 'react-bootstrap';
+
+function Navigation() {
+  return (
+    <nav className='navbar'>
+      <Container id='nav-container'>
+        <div className='navbar-brand'>
+          <Logo />
+        </div>
+        <div className='navbar-menu'>
+          <a className='navbar-item' href='#0'>Projects</a>
+          <a className='navbar-item' href='#0'>Resume</a>
+          <a className='navbar-item' href='#0'>Say Hello</a>
+        </div>
+      </Container>
+      </nav>
+  );
+}
+
+export default Navigation;
