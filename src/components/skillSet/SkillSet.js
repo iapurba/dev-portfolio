59fb7f887a4934f00f98d2f2fbfd61732d5946a8
Apurba Panja <apurbapanja1@gmail.com>
2020-11-28 23:34:06 +0530

    Updated static to dynamic

--- src/components/skillSet/SkillSet.js
+++ src/components/skillSet/SkillSet.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import SkillCard from '../skillCard/SkillCard';
 import './SkillSet.css';
 
 const SkillSet = () => {
@@ -7,31 +8,10 @@ const SkillSet = () => {
     <section className='skills'>
       <Container>
         <div className='skill-set shadow-sm'>
-        <div className='box-parent'>
-          <div className='box'>
-            <h3 className='title'>Front-end</h3>
-            <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
-            <p><strong>Languages</strong></p>
-            <p>HTML, CSS, JavaScript</p>
-            <p><strong>Dev tools</strong></p>
-            <p>React, Bootstrap, Tachyons, VSCode, Sublime, Atom</p>
-          </div>
-          <div className='box'>
-            <h3 className='title'>Back-end</h3>
-            <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
-            <p><strong>Languages</strong></p>
-            <p>Python, JavaScript(Nodejs)</p>
-            <p><strong>Dev tools</strong></p>
-            <p>Djnago, Express, Postman, Git, Github, Pycharm, Spyder, Jupiter Notebook</p>
-          </div>
-          <div className='box'>
-            <h3 className='title'>DBMS</h3>
-            <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
-            <p><strong>Languages</strong></p>
-            <p>SQL</p>
-            <p><strong>Dev tools</strong></p>
-            <p>Postgress, MySQL, SQLite, Valentina Studio</p>
-          </div>
+          <div className='box-parent'>
+            <SkillCard />
+            <SkillCard />
+            <SkillCard />
           </div>
         </div>
       </Container>
