665646d4e052f9ebb7ace64643897d12eaf26564
Apurba Panja <apurbapanja1@gmail.com>
2020-11-28 23:31:43 +0530

    Modified

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Logo.css'
-import logo from './logo.png';
+import brandLogo from './logo.svg';
 
 
 const Logo = () => {
   return (
     <div className='navbar-brand'>
       <a href='#0'>
-        <img alt='Apurba Panja Logo' src={logo} />
+        <img alt='Apurba Panja Logo' src={brandLogo} />
       </a>
     </div>
   );
