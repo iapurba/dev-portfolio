34646b4ead6710b4fbd87aab7a85c3cb4767a746
Apurba Panja <apurbapanja1@gmail.com>
2020-11-28 23:37:11 +0530

    Modified

--- src/components/projectCardList/ProjectCardList.js
+++ src/components/projectCardList/ProjectCardList.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import ProjectCard from '../projectCard/ProjectCard';
-import { Container } from 'react-bootstrap';
 import './ProjectCardList.css'
 
+const projects = [
+  {
+    name: "RoboFriends",
+    description: "Search your favourite robots.I promose you will love this.",
+    url: "https://iapurba.github.io/robofriends/",
+    sourceCode: "https://github.com/iapurba/robofriends"
+  },
+  {
+    name: "Smart Brain",
+    description: "This magic brain detect faces on picture. Give it a try.",
+    url: "https://facedetectingbrain.herokuapp.com/",
+    sourceCode: "https://github.com/iapurba/face-detection-brain"
+  },
+  {
+    name: "RoboFriends",
+    description: "Search your favourite robots",
+    url: "",
+    sourceCode: ""
+  },
+  {
+    name: "RoboFriends",
+    description: "Search your favourite robots",
+    url: "",
+    sourceCode: ""
+  },
+  {
+    name: "RoboFriends",
+    description: "Search your favourite robots",
+    url: "",
+    sourceCode: ""
+  },
+  {
+    name: "RoboFriends",
+    description: "Search your favourite robots",
+    url: "",
+    sourceCode: ""
+  },
+]
 
-const ProjectCardList = () => {
+const ProjectCardList = (projects) => {
+  console.log('projects',projects);
   return (
     <div className='card-list'>
       <ProjectCard />
