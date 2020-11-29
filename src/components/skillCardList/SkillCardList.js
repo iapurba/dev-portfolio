17180c8bf724711129093608540f0b079bccf562
Apurba Panja <apurbapanja1@gmail.com>
2020-11-29 23:56:44 +0530

    Renamed SkillSet to SkillCardList

---
+++ src/components/skillCardList/SkillCardList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import SkillCard from '../skillCard/SkillCard';
+import './SkillCardList.css';
+import { skills } from '../../assets/skillData';
+
+const SkillCardList = () => {
+  const skillList = skills.map((skill, i) => {
+    return (
+      <SkillCard
+        key={i}
+        icon={skill.icon}
+        title={skill.title}
+        description={skill.description}
+        languages={skill.languages}
+        frameworks={skill.frameworks}
+        tools={skill.tools}
+      />
+    )
+  })
+  return (
+    <section className='skills'>
+      <Container>
+        <div className='skill-body shadow-sm'>
+          <div className='box-parent'>
+            {skillList}
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default SkillCardList;
