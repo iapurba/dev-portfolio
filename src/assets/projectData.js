fb67c6821454868680ab22f6ce15b693c63d96c4
Apurba Panja <apurbapanja1@gmail.com>
2020-11-29 23:51:23 +0530

    Add images, data what is needed

---
+++ src/assets/projectData.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    name: "RoboFriends",
+    description: "Search your favourite robots.I promose you will love this.",
+    url: "https://iapurba.github.io/robofriends/",
+    sourceCode: "https://github.com/iapurba/robofriends"
+  },
+  {
+    name: "Face Detection Brain",
+    description: "This magic brain helps to detect faces on picture. Give it a try.",
+    url: "https://facedetectingbrain.herokuapp.com/",
+    sourceCode: "https://github.com/iapurba/face-detection-brain"
+  },
+  {
+    name: "Quotes Generator",
+    description: "Generates random quotes using API. You can also tweet them if you like.",
+    url: "https://iapurba.github.io/quote-generator/",
+    sourceCode: "https://github.com/iapurba/quote-generator"
+  },
+]
---
+++ src/assets/skillData.js
@@ -0,0 +1,28 @@
+import { faDatabase, faServer, faLaptopCode } from '@fortawesome/free-solid-svg-icons';
+
+export const skills = [
+  {
+    icon: faLaptopCode,
+    title: "Front-end",
+    description: "I like to code things from scratch, and enjoy bringing ideas to life in the browser.",
+    languages: "HTML, CSS, JavaScript",
+    frameworks: "React, Redux, Bootstrap, Tachyons",
+    tools: "VSCode, Atom"
+  },
+  {
+    icon: faServer,
+    title: "Back-end",
+    description: "When someone requests me someting I couldn't wait to response. ",
+    languages: "Python, JavaScript(NodeJs)",
+    frameworks: "Django, Node, Express",
+    tools: "Pycharm, Postman, Sublime"
+  },
+  {
+    icon: faDatabase,
+    title: "Database",
+    description: "Data is everything. Keeping it safe, secure and organized is my priority I reckon.",
+    languages: "SQL",
+    frameworks: "",
+    tools: "Postgres, SQLite, KnexJs, Valentina Studio"
+  }
+]
