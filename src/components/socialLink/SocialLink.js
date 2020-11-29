f7c90feb103ed3ea7508021654cb77ec44cb1254
Apurba Panja <apurbapanja1@gmail.com>
2020-11-29 23:57:13 +0530

    Modified

--- src/components/socialLink/SocialLink.js
+++ src/components/socialLink/SocialLink.js
@@ -7,19 +7,19 @@ import { faLinkedinIn, faFacebookF, faGithub, faInstagram, faTwitter } from '@fo
 const SocialLink = () => {
   return (
     <div className='social-links'>
-      <a href='https://www.linkedin.com/in/apurba-panja-bb88ab1a5/' target='blank'>
+      <a href='https://www.linkedin.com/in/apurba-panja-bb88ab1a5/' target='_blank'>
         <FontAwesomeIcon className='social-icon' icon={faLinkedinIn}/>
       </a>
-      <a href='https://github.com/iapurba' target='blank'>
+      <a href='https://github.com/iapurba' target='_blank'>
         <FontAwesomeIcon className='social-icon' icon={faGithub} />
       </a>
-      <a href='#0' target='blank'>
+      <a href='https://www.facebook.com/apurbapanja30/' target='_blank'>
         <FontAwesomeIcon className='social-icon' icon={faFacebookF} />
       </a>
-      <a href='https://twitter.com/apurba_panja' target='blank'>
+      <a href='https://twitter.com/apurba_panja' target='_blank'>
         <FontAwesomeIcon className='social-icon' icon={faTwitter} />
       </a>
-      <a href='https://www.instagram.com/iam_apurba_/' target='blank'>
+      <a href='https://www.instagram.com/iam_apurba_/' target='_blank'>
         <FontAwesomeIcon className='social-icon' icon={faInstagram} />
       </a>
     </div>
