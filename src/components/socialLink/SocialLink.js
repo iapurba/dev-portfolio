4394637de46b1736377ba1ccc8e4e19a09784429
Apurba Panja <apurbapanja1@gmail.com>
2020-11-29 15:52:50 +0530

    Updated

--- src/components/socialLink/SocialLink.js
+++ src/components/socialLink/SocialLink.js
@@ -7,19 +7,19 @@ import { faLinkedinIn, faFacebookF, faGithub, faInstagram, faTwitter } from '@fo
 const SocialLink = () => {
   return (
     <div className='social-links'>
-      <a href='#0'>
+      <a href='https://www.linkedin.com/in/apurba-panja-bb88ab1a5/' target='blank'>
         <FontAwesomeIcon className='social-icon' icon={faLinkedinIn}/>
       </a>
-      <a href='#0'>
+      <a href='https://github.com/iapurba' target='blank'>
         <FontAwesomeIcon className='social-icon' icon={faGithub} />
       </a>
-      <a href='#0'>
+      <a href='#0' target='blank'>
         <FontAwesomeIcon className='social-icon' icon={faFacebookF} />
       </a>
-      <a href='#0'>
+      <a href='https://twitter.com/apurba_panja' target='blank'>
         <FontAwesomeIcon className='social-icon' icon={faTwitter} />
       </a>
-      <a href='#0'>
+      <a href='https://www.instagram.com/iam_apurba_/' target='blank'>
         <FontAwesomeIcon className='social-icon' icon={faInstagram} />
       </a>
     </div>
