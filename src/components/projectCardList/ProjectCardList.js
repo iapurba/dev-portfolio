3796cead6cd03be8d46e02c156fd4fe6e17d5b8b
Apurba Panja <apurbapanja1@gmail.com>
2020-11-29 15:56:21 +0530

    Updated

--- src/components/projectCardList/ProjectCardList.js
+++ src/components/projectCardList/ProjectCardList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectCard from '../projectCard/ProjectCard';
+import { Container } from 'react-bootstrap';
 import './ProjectCardList.css'
 
 const projects = [
@@ -10,48 +11,41 @@ const projects = [
     sourceCode: "https://github.com/iapurba/robofriends"
   },
   {
-    name: "Smart Brain",
-    description: "This magic brain detect faces on picture. Give it a try.",
+    name: "Face Detection Brain",
+    description: "This magic brain helps to detect faces on picture. Give it a try.",
     url: "https://facedetectingbrain.herokuapp.com/",
     sourceCode: "https://github.com/iapurba/face-detection-brain"
   },
   {
-    name: "RoboFriends",
-    description: "Search your favourite robots",
-    url: "",
-    sourceCode: ""
-  },
-  {
-    name: "RoboFriends",
-    description: "Search your favourite robots",
-    url: "",
-    sourceCode: ""
-  },
-  {
-    name: "RoboFriends",
-    description: "Search your favourite robots",
-    url: "",
-    sourceCode: ""
-  },
-  {
-    name: "RoboFriends",
-    description: "Search your favourite robots",
-    url: "",
-    sourceCode: ""
+    name: "Quotes Generator",
+    description: "Generates random quotes using API. You can also tweet them if you like.",
+    url: "https://iapurba.github.io/quote-generator/",
+    sourceCode: "https://github.com/iapurba/quote-generator"
   },
 ]
 
-const ProjectCardList = (projects) => {
-  console.log('projects',projects);
+const ProjectCardList = () => {
+  const cardList = projects.map((project, i) => {
+    return (
+      <ProjectCard
+        key={i}
+        name={project.name}
+        description={project.description}
+        url={project.url}
+        sourceCode={project.sourceCode}
+      />
+    )
+  })
   return (
-    <div className='card-list'>
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-    </div>
+    <section className='projects'>
+      <Container>
+        <h1 className='title'><strong>My Recent Proects </strong></h1>
+        <p className='subtitle'>Here are my few web projects I have worked on.</p>
+        <div className='card-list'>
+          {cardList}
+        </div>
+      </Container>
+    </section>
   )
 }
 
