bbeca8e8ee49618329ad72fe1a34bdbae1b7d382
Apurba Panja <apurbapanja1@gmail.com>
2020-11-29 23:52:42 +0530

    Move App  to conatiners

---
+++ src/containers/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Navigation from '../components/navigation/Navigation';
+import HeroSection from '../components/heroSection/HeroSection';
+import About from '../components/about/About';
+import SkillCardList from '../components/skillCardList/SkillCardList';
+import ProjectCardList from '../components/projectCardList/ProjectCardList';
+import CallToAction from '../components/callToAction/CallToAction';
+import Footer from '../components/footer/Footer';
+
+import profleImage from '../assets/profileImage.jpg';
+import brandLogo from '../assets/brandLogo2.png';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <HeroSection profileImage={profleImage} />
+      <About />
+      <SkillCardList />
+      <ProjectCardList />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
