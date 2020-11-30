ba7b8787c2801152176d5330af0a26cbb904f409
Apurba Panja <apurbapanja1@gmail.com>
2020-11-30 12:19:34 +0530

    Modified contents

--- src/components/callToAction/CallToAction.js
+++ src/components/callToAction/CallToAction.js
@@ -9,14 +9,14 @@ const CallToAction = () => {
         <div className='call-to-action-box shadow'>
         <div className='level'>
           <div className='level-item'>
-            <h2>Start a project </h2>
+            <h2>Let's chat</h2>
           </div>
           <div className='level-item'>
-            <p>Interested in working together? We should queue up a chat. I’ll buy the coffee.</p>
+            <p>Have a project you would like me to work on? Give me a call or drop me an email.</p>
           </div>
           <div className='level-item'>
-            <h5>iamapurbax@gmail.com</h5>
             <h5>+91 9051555135</h5>
+            <h5>iamapurbax@gmail.com</h5>
           </div>
         </div>
         </div>
